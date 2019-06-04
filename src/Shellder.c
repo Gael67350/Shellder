e06e7883c866727f55d1f3c290b41d70267d8d40
Unknown <gossetg91@gmail.com>
2019-06-04 09:52:37 +0200

    finalisation du chargement de commande + début de gestion des programmes internes

--- src/Shellder.c
+++ src/Shellder.c
@@ -17,12 +17,21 @@ int main(int argc, char**argv)
   printf("     WELCOME TO SHELLDER V 0.1      \n");
   printf("====================================\n");
 
-  display shellder here
+  //display shellder here
 
   bool exitPerformed = false;
 
   while(!exitPerformed)
   {
+    struct CommandEntry loadedCommand;
+    
     promptDisplay();
+    loadedCommand = readCommand();
+
+    if(strcmp(loadedCommand.programName,"cd")==0)
+    {
+      printf("cd performed \n");
+    }
+    
   }
 }
--- src/commandEntryComponents/entryManagement.c
+++ src/commandEntryComponents/entryManagement.c
@@ -19,7 +19,7 @@ void promptDisplay()
   gethostname(hostName,hostSize);
   getcwd(currentPath,pathSize);
   
-  printf("%s@%s : %s >>",getlogin(),hostName,currentPath); 
+  printf("%s@%s : %s >> ",getlogin(),hostName,currentPath); 
 
 }
 
@@ -28,41 +28,49 @@ CommandEntry readCommand()
   char buffer[10];
 
   char* finalCommand = malloc(sizeof(char));
-
-  printf(">>> ");
-
-    do
+  finalCommand[0] = '\0';
+  
+  int readed;
+  do
+  {
+    fgets(buffer,10,stdin);
+    readed = strlen(buffer);
+    
+    if(buffer[strlen(buffer)-1] == '\n' && buffer[strlen(buffer)-2] != '\\')
     {
-      fgets(buffer,10,stdin);
-      if(buffer[strlen(buffer)-1] == '\n' && buffer[strlen-2] != '\\')
-      {
-	buffer[strlen(buffer-1)] = '\0';
-      }
-      fgets(buffer,10,stdin);
-      if(buffer[strlen(buffer)-1] == '\n' && buffer[strlen-2] != '\\')
-      {
-	buffer[strlen(buffer-1)] = '\0';
-      }
-      
-      finalCommand = realloc((void*)finalCommand,strlen(finalCommand)*sizeof(char)+strlen(buffer)*sizeof(char));
-
-      strcat(finalCommand,buffer);
-
-    }while(buffer[strlen(buffer)-1] != '\0');
-    
-    printf("%s",finalCommand);
-    
-    char* programName = strtok(finalCommand,' ');
-    
-    char* newParameter;
-    char** parameters = malloc(1*sizeof(char*));
-    int parameterCount = 0;
-    
-    while((newParameter = strtok(NULL,' ')) != NULL)
+      buffer[strlen(buffer)-1] = '\0';
+    }
+    else if(buffer[strlen(buffer)-1] == '\n' && buffer[strlen(buffer)-2] == '\\')
     {
-      parameterCount ++;
-      printf(newParameter);
+      buffer[strlen(buffer)-2] = ' ';
+      buffer[strlen(buffer)-1] = '\0';
+      readed = strlen(buffer);
     }
 
-    free(finalCommand);
+    finalCommand = realloc((void*)finalCommand,(strlen(finalCommand)+strlen(buffer))*sizeof(char));
+    strcat(finalCommand,buffer);
+    
+  }while(readed == strlen(buffer));
+
+  char* programName = strtok(finalCommand," ");
+  
+  char* newParameter;
+  char** parameters = malloc(sizeof(char*));
+  int parameterCount = 0;
+    
+  while((newParameter = strtok(NULL," ")) != NULL)
+  {
+    parameters[parameterCount] = newParameter;
+    parameterCount ++;
+    parameters = realloc((void*)parameters,(parameterCount+1)*sizeof(char*));
+  }
+
+  struct CommandEntry buildedCommand;
+  buildedCommand.programName = programName;
+  buildedCommand.parameters = parameters;
+  buildedCommand.parameterCount = parameterCount;
+  
+  return buildedCommand;
+
 }
+
